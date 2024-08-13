be390143e72b828567f537d618615fb790d98a2f
nascjoao <oi@nasc.dev>
2024-08-13 15:42:32 -0300

    refactor: move stories to dedicated directory

--- src/stories/Player.stories.ts
+++ src/stories/Player.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Player from ".";
+import { Player } from "@harpa-player";
 
 const meta = {
     title: "Example/Player",
--- src/stories/player-context.stories.tsx
+++ src/stories/player-context.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { Meta, StoryFn } from "@storybook/react";
-import { PlayerProvider } from ".";
-import { usePlayer } from "../..";
+import { usePlayer, PlayerProvider } from "@harpa-player";
 
 const meta: Meta = {
     title: "Contexts/Player",
