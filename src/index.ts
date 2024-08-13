eee686896e3d177a43de595776d59137a2204fad
nascjoao <oi@nasc.dev>
2024-08-13 15:31:47 -0300

    chore: Update index.ts to export PlayerProvider and usePlayer hook

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import React from "react";
+import { PlayerContext } from "./contexts/player";
+
 export { default as Player } from "./Player";
+
+export { PlayerProvider } from "./contexts/player";
+
+export const usePlayer = () => {
+    return React.useContext(PlayerContext);
+};
